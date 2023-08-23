efad228ba3df5490a2ec955e60a43e15385d5e38
Gubscruz <124577208+Gubscruz@users.noreply.github.com>
2023-08-23 12:06:09 -0300

    ex 3

--- main_1.py
+++ main_1.py
@@ -3,10 +3,8 @@ import sqlite3
 conn = sqlite3.connect('database_alunos.db')
 cursor = conn.cursor()
 
-# Deleta a tabela "Estudantes" se ela já existir
 cursor.execute("DROP TABLE IF EXISTS Estudantes")
 
-# Recria a tabela "Estudantes"
 cursor.execute("""
 CREATE TABLE Estudantes (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,7 +13,7 @@ CREATE TABLE Estudantes (
     AnoIngresso INTEGER
 );
 """)
-# Inserção dos registros
+
 estudantes = [
     ('Ana Silva', 'Computação', 2019),
     ('Pedro Mendes', 'Física', 2021),
@@ -31,23 +29,19 @@ VALUES (?, ?, ?);
 
 conn.commit()
 
-# Seleção e impressão de todos os estudantes
 cursor.execute('SELECT * FROM Estudantes')
 print("Todos os estudantes:")
 for registro in cursor.fetchall():
     print(registro)
 
-# Filtrar e mostrar os estudantes que ingressaram entre 2019 e 2020
 cursor.execute('SELECT * FROM Estudantes WHERE AnoIngresso BETWEEN 2019 AND 2020')
 print("\nEstudantes que ingressaram entre 2019 e 2020:")
 for registro in cursor.fetchall():
     print(registro)
 
-# Atualizar o "Ano de Ingresso" de Ana Silva, por exemplo
 cursor.execute('UPDATE Estudantes SET AnoIngresso = ? WHERE Nome = ?', (2023, 'Ana Silva'))
 conn.commit()
 
-# Seleção e impressão de todos os estudantes novamente
 
 cursor.execute('DELETE FROM Estudantes WHERE ID = ?', ('4',))
 conn.commit()
